e118fd831d5545d9666b3c24e810f817938eeb48
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2015-12-10 11:39:15 +0100

    other tests for the view

--- swtbot.example.tests/src/swtbot/example/tests/SampleViewTest.java
+++ swtbot.example.tests/src/swtbot/example/tests/SampleViewTest.java
@@ -1,10 +1,13 @@
 package swtbot.example.tests;
 
+import static org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses;
+
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
@@ -41,6 +44,44 @@ public class SampleViewTest {
 	@Test
 	public void testSampleViewInJavaPerspective() {
 		bot.viewByTitle("Sample View").show();
+	}
+
+	@Test
+	public void testViewTree() {
+		SWTBotView view = bot.viewByTitle("Sample View");
+		view.bot().tree().getTreeItem("Root").expand();
+		view.bot().tree().getTreeItem("Root").getNode("Parent 1").expand();
+		view.bot().tree().getTreeItem("Root").getNode("Parent 1").getNode("Leaf 1").select();
+	}
+
+	@Test
+	public void testViewTreeDoubleClick() {
+		SWTBotView view = bot.viewByTitle("Sample View");
+		view.bot().tree().getTreeItem("Root").doubleClick();
+		assertDialog("Double-click detected on Root");
+	}
+
+	@Test
+	public void testViewToolbar() {
+		bot.toolbarButtonWithTooltip("Action 1 tooltip").click();
+		assertDialog("Action 1 executed");
+		bot.toolbarButtonWithTooltip("Action 2 tooltip").click();
+		assertDialog("Action 2 executed");
+	}
+
+	@Test
+	public void testViewTreeContextMenu() {
+		SWTBotView view = bot.viewByTitle("Sample View");
+		view.bot().tree().getTreeItem("Root").contextMenu("Action 1").click();
+		assertDialog("Action 1 executed");
+	}
+
+	private void assertDialog(String labelInDialog) {
+		SWTBotShell dialog = bot.shell("Sample View");
+		dialog.activate();
+		bot.label(labelInDialog);
+		bot.button("OK").click();
+		bot.waitUntil(shellCloses(dialog));
 	}
 
 	private static void openJavaPerspective() throws InterruptedException {
